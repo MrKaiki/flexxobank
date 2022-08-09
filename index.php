1b028bc850b11296f5f849aae8f51b3c2113d6d2
Kaiki <dalvamso60@gmail.com>
2022-08-09 20:19:21 -0300

    Desenvolviemento WEB

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Flexxo BANK</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container">
+    <a class="navbar-brand" href="#"><img src="blaze.png" alt="" width="90" height="40"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Institucional</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link">Criar Conta</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+    
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container py-5">
+        <h1 class="display-5 fw-bold">Blaze Bank</h1>
+        <p class="col-md-8 fs-4">Seu banco mais flexivel do que nunca!</p>
+        <a class="btn btn-primary btn-lg" href="criar_conta.php">Crie sua conta</a>
+      </div>
+    </div>
+
+    <div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Pagina Inicial</a></li>
+            <li class="nav-item"><a href="institucional.php" class="nav-link px-2 text-muted">Institucional</a></li>
+            <li class="nav-item"><a href="criar_conta.php" class="nav-link px-2 text-muted">Crie sua conta</a></li>
+            </ul>
+            <p class="text-center text-muted">&copy; 2022 Blaze BANK</p>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
