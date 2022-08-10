38823681ad1ec9979804e831da6640f84d360145
Kaiki <dalvamso60@gmail.com>
2022-08-09 22:04:05 -0300

    Atualização

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Pagina Inicial</a></li>
+            <li class="nav-item"><a href="institucional.php" class="nav-link px-2 text-muted">Institucional</a></li>
+            <li class="nav-item"><a href="criar_conta.php" class="nav-link px-2 text-muted">Crie sua conta</a></li>
+            </ul>
+            <p class="text-center text-muted">&copy; 2022 Blaze BANK</p>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
